0103b491c6842b4f9dc3b644cf214efdaf854f55
yaroslavsoroka97 <19yariksoroka97@gmail.com>
2021-12-17 15:20:29 +0200

    Init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x, y, a, w, b, X, e;
+        x = 6;
+        y = 10;
+        a = 14;
+        w = 8;
+        b = 18;
+        X = 2;
+        e = 8;
+        double result1, result2, result3, result4;
+        result1 = x + y - a / 2 * (x + w);
+        result2 = a * (x + x) / y - a * 3;
+        result3 = a + b / x - y;
+        result4 = X - e / 2 % e + 1;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
